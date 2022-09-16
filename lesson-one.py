9d4c4f8c321641e64c8b12d078bfd1f577d6ab8a
Siyamak Abbasnezhad <siyamak1981@gmail.com>
2022-09-16 13:04:15 +0430

    code of github

--- lesson-one.py
+++ lesson-one.py
@@ -1,3 +1,6 @@
+from mimetypes import init
+
+
 x = "programmer" #x = x is globale
 def foo():
     x = "xxxx" #x is local
@@ -31,38 +34,43 @@ print("python is " + x )
 
 
 #------------------------------------------------#
-bin()
-abs()
-alter()
-if
-in
-is
-bool
-True
-False
-enumerate()
-zip()
-id()
-type()
-print()
-vars()
-round()
-range()
-all()
-any()
-__import__()
-tuple()
-open()
-set()
-setattr()
-slice()
-str()
-float()
-format()
-filter()
-super()
-locals()
+# bin()
+# abs()
+# alter()
+# if
+# in
+# is
+# bool
+# True
+# False
+# enumerate()
+# zip()
+# id()
+# type()
+# print()
+# vars()
+# round()
+# range()
+# all()
+# any()
+# __import__()
+# tuple()
+# open()
+# set()
+# setattr()
+# slice()
+# str()
+# float()
+# format()
+# filter()
+# super()
+# locals()
+
+# git init
+# git add .
+# git commit -m "explanation"
+# git push origin master
 
 
-
-
+# mahdy.mohseni09022587508@gmail.com
+# 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
